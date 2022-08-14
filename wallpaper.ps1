cf879e5154ba3627114d0fffdb79e7ea98707e89
bogoch <63978357+bogoch@users.noreply.github.com>
2022-08-14 03:40:33 +0300

    Add files via upload

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -1,22 +1,5 @@
-DELAY 500
-GUI r
-DELAY 500
-STRING powershell
-DELAY 200
-ENTER
-DELAY 500
-STRING Invoke-WebRequest "https://ih1.redbubble.net/image.2675223293.8921/flat,750x,075,f-pad,750x1000,f8f8f8.jpg" -OutFile test.jpg
-DELAY 300
-ENTER
-DELAY 300
-STRING Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "%USERPROFILE%\test.jpg"
-DELAY 300
-ENTER
-DELAY 300
-STRING $value=0; while ($value -ne 15){Start-Sleep -Seconds 1; $value = $value + 1;rundll32.exe user32.dll, UpdatePerUserSystemParameters }
-DELAY 300
-ENTER
-DELAY 300
-STRING exit
-DELAY 300
-ENTER
+Invoke-WebRequest "https://ih1.redbubble.net/image.2675223293.8921/flat,750x,075,f-pad,750x1000,f8f8f8.jpg" -OutFile test.jpg;
+Start-Sleep -Seconds 1
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "%USERPROFILE%\test.jpg";
+Start-Sleep -Seconds 1
+rundll32.exe user32.dll, UpdatePerUserSystemParameters;
