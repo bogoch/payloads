1130ce86615cad6fb9c5c3ab9c63d730feb42261
bogoch <63978357+bogoch@users.noreply.github.com>
2022-08-15 16:33:09 +0300

    Update wallpaper.ps1

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -1,10 +1,15 @@
 Invoke-WebRequest "https://ih1.redbubble.net/image.2675223293.8921/flat,750x,075,f-pad,750x1000,f8f8f8.jpg" -OutFile test.jpg;
 Start-Sleep -Seconds 1
-Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "%USERPROFILE%\test.jpg";
-While ($true)
+
+
+while ($true)
 {
-  rundll32.exe user32.dll, UpdatePerUserSystemParameters;
-  Start-Sleep -Seconds 3
-
+  Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "%USERPROFILE%\test.jpg";
+  $counter = 0;
+  While ($counter -eq 15)
+  {
+    rundll32.exe user32.dll, UpdatePerUserSystemParameters;
+    Start-Sleep -Seconds 3
+    $counter += 1;
+  }
 }
-
