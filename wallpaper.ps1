9f516b9f2804d11d1251dd1697d5751f9fa4af31
bogoch <63978357+bogoch@users.noreply.github.com>
2022-08-15 14:07:06 +0300

    Update wallpaper.ps1
    changed to while loop

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -1,5 +1,10 @@
 Invoke-WebRequest "https://ih1.redbubble.net/image.2675223293.8921/flat,750x,075,f-pad,750x1000,f8f8f8.jpg" -OutFile test.jpg;
 Start-Sleep -Seconds 1
 Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "%USERPROFILE%\test.jpg";
-Start-Sleep -Seconds 1
-rundll32.exe user32.dll, UpdatePerUserSystemParameters;
+While ($true)
+{
+  rundll32.exe user32.dll, UpdatePerUserSystemParameters;
+  Start-Sleep -Seconds 3
+
+}
+
