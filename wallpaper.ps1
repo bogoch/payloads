1abd512e297bd8082afe11f5cefd1ddfaa3c9032
bogoch <63978357+bogoch@users.noreply.github.com>
2022-08-15 20:02:43 +0300

    Update wallpaper.ps1

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -4,23 +4,26 @@ Start-Sleep -Seconds 5
 
 While ($true)
 {
-  $imgPath="$env:USERPROFILE\test.jpg"
-  $code = @' 
-  using System.Runtime.InteropServices; 
-  namespace Win32{ 
+$imgPath="$env:USERPROFILE\test.jpg"
+$code = @' 
+using System.Runtime.InteropServices; 
+namespace Win32{ 
+    
+     public class Wallpaper{ 
+        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+         
+         public static void SetWallpaper(string thePath){ 
+            SystemParametersInfo(20,0,thePath,3); 
+         }
+    }
+ } 
+'@
 
-       public class Wallpaper{ 
-          [DllImport("user32.dll", CharSet=CharSet.Auto)] 
-           static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+add-type $code 
 
-           public static void SetWallpaper(string thePath){ 
-              SystemParametersInfo(20,0,thePath,3); 
-           }
-      }
-   }'@
+#Apply the Change on the system 
+[Win32.Wallpaper]::SetWallpaper($imgPath)
 
-  add-type $code 
-  #Apply the Change on the system 
-  [Win32.Wallpaper]::SetWallpaper($imgPath)
   Start-Sleep -Seconds 3
 }
