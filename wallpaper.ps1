ef905ae945efc7036c9d7523f4fe4ce644b176cc
bogoch <63978357+bogoch@users.noreply.github.com>
2022-08-15 17:07:26 +0300

    Update wallpaper.ps1

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -1,10 +1,11 @@
+cd $env:USERPROFILE
 Invoke-WebRequest "https://ih1.redbubble.net/image.2675223293.8921/flat,750x,075,f-pad,750x1000,f8f8f8.jpg" -OutFile test.jpg;
 Start-Sleep -Seconds 1
 
 
 while ($true)
 {
-  Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "%USERPROFILE%\test.jpg";
+  Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "$env:USERPROFILE\test.jpg";
   $counter = 0;
   While ($counter -eq 15)
   {
